5f117891e6b6e1f42624888ffc5fbc297bb880c5
Florent Kowalczuk <florent.kowalczuk@gmail.com>
2025-07-06 20:04:23 +0900

    corrections bugs

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -1,6 +1,7 @@
 'use client'
 import SignIn from '@/app/components/SignIn'
 import React, { useEffect } from 'react';
+import { SessionProvider } from "next-auth/react";
 
 export default function SignInPage() {
     
